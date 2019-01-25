1361a8703dda4fb28deff1b327b592c91b10a30a
aaa4024940 <1255295191@qq.com>
2019-01-25 17:09:27 +0800

    'test'

--- Scripts/test_sms.py
+++ Scripts/test_sms.py
@@ -1,11 +1,11 @@
 
-class app_day08:
+class Test_day08:
 
-    def app_test01(self):
+    def test_test01(self):
         print("--->app_test01")
         assert True
 
-    def app_test02(self):
+    def test_test02(self):
         print("--->app_test02")
         assert False
 
