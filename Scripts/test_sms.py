858ad27fedbeab20fda181de278b3cc99fd82593
aaa4024940 <1255295191@qq.com>
2019-01-25 17:05:49 +0800

    '123'

---
+++ Scripts/test_sms.py
@@ -0,0 +1,11 @@
+
+class app_day08:
+
+    def app_test01(self):
+        print("--->app_test01")
+        assert True
+
+    def app_test02(self):
+        print("--->app_test02")
+        assert False
+
